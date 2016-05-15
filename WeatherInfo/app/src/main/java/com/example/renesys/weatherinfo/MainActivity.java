ea2174b2c5f3a2a3b490f899c7781e5f9b2cf43f
RENESYS <hrs350x@gmail.com>
2016-05-15 16:05:12 +0900

    time select function add

--- WeatherInfo/app/src/main/java/com/example/renesys/weatherinfo/MainActivity.java
+++ WeatherInfo/app/src/main/java/com/example/renesys/weatherinfo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.renesys.weatherinfo;
 
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,30 +13,37 @@ public class MainActivity extends AppCompatActivity {
     private Weather w = new Weather();
     private TextView txt;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt = (TextView)findViewById(R.id.text);
+        final LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
         new Thread(){
             public void run(){
-                w.setURL();
+                w.setURL(lm);
 
+                Bundle bun = new Bundle();
+                bun.putString("data", w.getLine());
+                Message msg = han.obtainMessage();
+                msg.setData(bun);
+                han.sendMessage(msg);
             }
         }.start();
 
-        System.out.println("asdasd : " + w.getLine());
+        txt = (TextView)findViewById(R.id.text);
     }
 
-    public void func(){
-
-    }
-
-    Handler handler = new Handler(){
-        public void handleMessage(Weather w){
-            txt.setText(w.getLine());
+    Handler han = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bun = msg.getData();
+            String str = bun.getString("data");
+            txt.setText(str);
         }
     };
 
 }
+
--- WeatherInfo/app/src/main/java/com/example/renesys/weatherinfo/Weather.java
+++ WeatherInfo/app/src/main/java/com/example/renesys/weatherinfo/Weather.java
@@ -1,5 +1,10 @@
 package com.example.renesys.weatherinfo;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
@@ -10,6 +15,10 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.StringTokenizer;
 
 /**
  * Created by RENESYS on 2016-05-13.
@@ -17,31 +26,25 @@ import java.net.URL;
 public class Weather {
 
     String line = "a";
+    double lon;
+    double lat;
+    private Location loca;
 
     public Weather(){
 
     }
 
-    public void setURL() {
+    public void setURL(LocationManager lm) {
         try {
-            System.out.println("asdf");
-            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lon=126.97&lat=37.56&APPID=b98538e3e4195e5390a18fbb31a2d57e");
+            getCoordinate(lon, lat, lm);
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast?lon=127&lat=37&APPID=b98538e3e4195e5390a18fbb31a2d57e");
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             InputStream in = new BufferedInputStream(conn.getInputStream());
             JSONObject json = new JSONObject(getStringFromInputStream(in));
 
-            JSONObject j = json.getJSONArray("list").getJSONObject(4);
-            String line = j.getJSONArray("weather").getJSONObject(0).getString("main");
+            JSONObject j = getForecast(json);
+            line = j.getJSONArray("weather").getJSONObject(0).getString("main");
             System.out.println(line);
-
-            //line = json.getJSONObject("list").toString();
-
-
-
-            //float temper = jo.getJSONObject("main").getInt("temp");
-            //String weather = jo.getJSONObject("weather").getString("main");
-
-
 
         } catch (MalformedURLException e) {
             System.out.println("URL ERROR");
@@ -57,6 +60,55 @@ public class Weather {
 
     public String getLine(){
         return line;
+    }
+
+    //get longitude & latitude from network base
+    private void getCoordinate(double lon, double lat, LocationManager lm){
+        try {
+            loca = lm.getLastKnownLocation(lm.NETWORK_PROVIDER);
+            lon = loca.getLongitude();
+            lat = loca.getLatitude();
+        }catch(SecurityException e){
+            e.printStackTrace();
+        }
+    }
+
+    //return the correct JSONObject based on current time
+    private JSONObject getForecast(JSONObject json) {
+        JSONObject j = new JSONObject();
+        String currentTime = getTime();
+        try {
+                for(int i = 0; ; i++){
+                    j = json.getJSONArray("list").getJSONObject(i);
+                    String time = j.getString("dt_txt");
+                    System.out.println(i);
+                    if(timeProfit(currentTime, time))
+                        break;
+                }
+
+
+        }catch(Exception e){
+
+        }
+        return j;
+    }
+
+    //make current time string
+    private String getTime(){
+        String format = new String("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat(format, Locale.KOREA);
+        return sdf.format(new Date());
+    }
+
+    //determine whether time is correct
+    private boolean timeProfit(String origin, String compare){
+        String[] tOrigin = origin.split("[- :]");
+        String[] tCompare = compare.split("[- :]");
+        int lOrigin = Integer.parseInt(tOrigin[1])*31*24 + Integer.parseInt(tOrigin[2])*24
+                + Integer.parseInt(tOrigin[3]);
+        int lCompare = Integer.parseInt(tCompare[1])*31*24 + Integer.parseInt(tCompare[2])*24
+                + Integer.parseInt(tCompare[3]);
+        return lOrigin < lCompare && lOrigin + 3 >= lCompare;
     }
 
     private static String getStringFromInputStream(InputStream is) {
